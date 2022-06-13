cb0ffedc49ede11c391c8ad31019d6d11d2ea7e9
lhunnewe <leroy.hunnewell@outlook.com>
2022-06-13 11:28:06 -0700

    2022-06-22

--- Quiz 10.py
+++ Quiz 10.py
@@ -2,6 +2,7 @@ from tkinter import *
 import os
 import random
 from tkinter import messagebox
+import tkinter
 
 
 root=Tk()
@@ -14,7 +15,7 @@ class menu:
         self.quizzes = []
         self.file_scan()
         
-        height = 500 #Height of the canvas
+        height = 600 #Height of the canvas
         width = 600 #Width of the canvas
         canvas = Canvas(root,height=height,width=width) #Declaring the height/width of canvas
         canvas.pack() #Packing the canvas
@@ -24,22 +25,22 @@ class menu:
         bg_label = Label(root,image=self.bg_image) #Assigning image to a label that is mapped to the root
         bg_label.place(relwid=1,relheight=1) #Placing the label to 100% of the canvas
 
-        frame_hdr = Frame(root,bg='gray',bd=5) #Creating a Frame for the header
-        frame_hdr.place(relx=0.0,rely=0.01,relwidth=1.0,relheight=0.1) #Placement of Frame, xy are defaulted to NW of the grid
+        self.frame_hdr = Frame(root,bg='gray',bd=5) #Creating a Frame for the header
+        self.frame_hdr.place(relx=0.0,rely=0.01,relwidth=1.0,relheight=0.1) #Placement of Frame, xy are defaulted to NW of the grid
 
-        label_hdr=Label(frame_hdr,text='The Wiz Quiz',font=('Times',18))#Label that goes inside the header with Text
+        label_hdr=Label(self.frame_hdr,text='The Wiz Quiz',font=('Times',18))#Label that goes inside the header with Text
         label_hdr.place(relx=0.00,rely=0,relwidth=1.0,relheight=0.95)#xy and relative width and height to the frame
         
         #frame_p
         self.frame_p=Frame(root,bg='gray',bd=5)
-        self.frame_p.place(relx=0.05,rely=0.15,relwidth=0.90,relheight=.75)
+        self.frame_p.place(relx=0.25,rely=0.15,relwidth=0.50,relheight=.55)
 
         #global pf_listbox 
         self.pf_listbox = Listbox(self.frame_p) #Listbox for showing the list of project files
         for i, p in enumerate(self.quizzes):
             self.pf_listbox.insert(i,p)
         #pf_listbox.bind('<<ListboxSelect>>',self.CurSelect())#Upon clicking, stores selected value RESEARCH THIS!
-        self.pf_listbox.place(relx=0,rely=0.15,relwidth=1.0,relheight=1.0)
+        self.pf_listbox.place(relx=0.0,rely=0.0,relwidth=1.0,relheight=.90)
 
         button_submit=Button(self.frame_p,font=('courier',10),text='Select Quiz',relief= 'groove', command=self.quiz_select)
         button_submit.place(relx=0.0,rely=0.90,relwidth=1.0,relheight=0.10)
@@ -59,6 +60,7 @@ class menu:
         bg_label = Label(root,image=self.bg_image) #Assigning image to a label that is mapped to the root
         bg_label.place(relwid=1,relheight=1) #Placing the label to 100% of the canvas
         self.frame_p.destroy()
+        self.frame_hdr.destroy()
         Q = quiz(active_quiz=quiz_sel)        
 
 class quiz:
@@ -66,6 +68,16 @@ class quiz:
 
     def __init__(self, active_quiz):
         """Initialize"""
+        
+        self.frame_p=Frame(root,bd=5)
+        self.frame_p.place(relx=0.05,rely=0.05,relwidth=0.90,relheight=.35)
+        
+        self.frame_b=Frame(root,bd=5)
+        self.frame_b.place(relx=0.05,rely=0.75,relwidth=0.90,relheight=.10)
+
+        # self.frame_result=Frame(root,bd=5) #How to hide frames???
+        # self.frame_result.place(relx=0.05,rely=0.05,relwidth=0.90,relheight=.35)
+
         self.active_quiz = active_quiz
         self.questions = []
         self.quiz_open(self.active_quiz)
@@ -76,32 +88,30 @@ class quiz:
         self.display_options(self.q_number)
         self.buttons()
         self.correct=0
-        
-        #self.quizzes = []
-        #self.stats = []
-
-        height = 500 #Height of the canvas
-        width = 600 #Width of the canvas
-        canvas = Canvas(root,height=height,width=width) #Declaring the height/width of canvas
-        canvas.pack() #Packing the canvas
+        # height = 500 #Height of the canvas
+        # width = 600 #Width of the canvas
+        # canvas = Canvas(root,height=height,width=width) #Declaring the height/width of canvas
+        # canvas.pack() #Packing the canvas 
     def question(self, q_number):
         #quiz_name= self.active_quiz[:-4]  #excludes csv extension in header
         #t = Label(root, text=quiz_name, width=50, bg="blue", fg="white", font=("times", 20, "bold"))
         #t.place(x=0, y=2)
-        qn = Label(root, text=self.questions[q_number][0], width=60, font=("times", 16, "bold"), anchor="w")
-        qn.place(x=70, y=100)
+        #qn = Label(root, text=self.questions[q_number][0], width=60, font=("times", 16, "bold"), anchor="w")
+        qn = Label(self.frame_p, text=self.questions[q_number][0], width=60, font=("times", 16, "bold"), anchor="w")
+        qn.place(x=70, y=10)
         return qn
     def radiobtns(self):
         val = 0
-        b = []
-        yp = 150
+        B = []
+        yp = 50
         while val < 4:
-            btn = Radiobutton(root, text=" ", variable=self.opt_selected, value=val + 1, font=("times", 14))
-            b.append(btn)
+            #btn = Radiobutton(root, text=" ", variable=self.opt_selected, value=val + 1, font=("times", 14))
+            btn = Radiobutton(self.frame_p, text=" ", variable=self.opt_selected, value=val + 1, font=("times", 14))
+            B.append(btn)
             btn.place(x=100, y=yp)
             val += 1
-            yp += 40
-        return b
+            yp += 25
+        return B
     def display_options(self, q_number):
         val = 0
         self.opt_selected.set(0)
@@ -111,10 +121,12 @@ class quiz:
             self.opts[val]['text'] = choice
             val +=1
     def buttons(self):
-        nbutton = Button(root, text="Next",command=self.nextbtn, width=10,bg="green",fg="white",font=("times",16,"bold"))
-        nbutton.place(x=200,y=380)
-        quitbutton = Button(root, text="Quit", command=root.destroy,width=10,bg="red",fg="white", font=("times",16,"bold"))
-        quitbutton.place(x=380,y=380)
+        nbutton = Button(self.frame_b, text="Next",command=self.nextbtn, width=10,bg="green",fg="white",font=("times",16,"bold"))
+        nbutton.pack(side=tkinter.LEFT)
+        # quitbutton = Button(root, text="Quit", command=root.destroy,width=10,bg="red",fg="white", font=("times",16,"bold"))
+        # quitbutton.place(x=380,y=380)
+        quitbutton = Button(self.frame_b, text="Quit", command=root.destroy,width=10,bg="red",fg="white", font=("times",16,"bold"))
+        quitbutton.pack(side=tkinter.RIGHT)
     
     def checkans(self, q_number):
         #print('Questions: ', self.questions[q_number], 'selected: ',self.opt_selected.get())
@@ -122,7 +134,8 @@ class quiz:
         choices = self.questions[q_number][2]
         selected_ans = choices[0][self.opt_selected.get()-1]
         if ans == selected_ans:
-            print(selected_ans, 'is correct.', 'correct answers: ',self.correct +1,'out of', len(self.questions))
+            num_questions = len(self.questions) -1
+            print(selected_ans, 'is correct.', 'correct answers: ',self.correct +1,'out of', num_questions)
             return True
         
     def nextbtn(self):
@@ -144,12 +157,16 @@ class quiz:
                 random.shuffle(choices)
                 self.questions.append([q,ans,[choices]])
     def display_result(self):
-        score = int(self.correct / len(self.questions) * 100)
+        # self.frame_p.destroy()
+        self.frame_result.tkraise()
+        num_question = len(self.questions) -1
+        score = int(self.correct / num_question * 100)
         result = "Score: " + str(score) + "%"
-        wc = len(self.questions) - self.correct
+        wc = num_question - self.correct
         correct = "No. of correct answers: " + str(self.correct)
         wrong = "No. of wrong answers: " + str(wc)
-        messagebox.showinfo("Result", "\n".join([result, correct, wrong]))
+        
+        # messagebox.showinfo("Result", "\n".join([result, correct, wrong]))
 
 
 M = menu()
